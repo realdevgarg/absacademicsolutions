af2f9d9bfa74e012f10eab7172166e0ee194b032
Dev <devgargperso@gmail.com>
2021-03-27 18:30:45 +0530

    script

--- dist/Js/script.js
+++ dist/Js/script.js
@@ -60,6 +60,7 @@ function setErrorFor(input, message) {
 
 // If there is no error, than what we want to do with input ?
 function setSuccessFor(input) {
+  handleSubmit();
   let formControl = input.parentElement;
   formControl.className = "input success";
 }
@@ -449,4 +450,27 @@ function changeHeaderNav(){
   }
 
 
+
+  // FORM SUBMISSION
+    var form = document.querySelector("form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+
+    // form.addEventListener("submit", handleSubmit)
 
