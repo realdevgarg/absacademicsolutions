cce2f2261c4742e736e35fb544055a3c9f384b6f
Dev <devgargperso@gmail.com>
2021-03-30 11:12:53 +0530

    a

--- dist/Js/main.js
+++ dist/Js/main.js
@@ -376,3 +376,25 @@ function topFunction() {
 
 
 document.querySelector('#top-nav-item').addEventListener('click', topFunction);
+
+
+// MOBILE HEADER 
+function openNav() {
+  document.getElementById("mySideNav").style.width = "100%";
+closeNavAuto();
+  }
+  
+  function closeNav() {
+  document.getElementById("mySideNav").style.width = "0";
+  }
+
+
+
+function closeNavAuto(){
+  let navs = document.querySelectorAll('.nav');
+  for(let i = 0; i < navs.length; i++){
+    navs[i].addEventListener('click', closeNav);
+  }
+}
+
+closeNavAuto();
