6450553be34c6387223ef9870a8a7a51cdf40e2e
Dev <devgargperso@gmail.com>
2021-03-30 10:18:55 +0530

    version-10

--- dist/Js/script.js
+++ dist/Js/script.js
@@ -2,8 +2,20 @@
 // MOBILE HEADER 
 function openNav() {
   document.getElementById("mySideNav").style.width = "100%";
+closeNavAuto();
   }
   
   function closeNav() {
   document.getElementById("mySideNav").style.width = "0";
   }
+
+
+
+function closeNavAuto(){
+  let navs = document.querySelectorAll('.nav');
+  for(let i = 0; i < navs.length; i++){
+    navs[i].addEventListener('click', closeNav);
+  }
+}
+
+closeNavAuto();
